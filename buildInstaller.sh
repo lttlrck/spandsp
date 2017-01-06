70df9c6ba8eed9e3e4da3a208d4840a883870045
Stuart Allen <stuart@valid8.com>
2017-01-06 10:04:56 -0800

    added rpms

--- buildInstaller.sh
+++ buildInstaller.sh
@@ -4,6 +4,7 @@ rm -rf ./installer
 mkdir ./installer
 mkdir -p ./installer/test-data/itu/fax/
 
+cp ./fftw*.rpm ./installer/
 cp ./tests/.libs/fax_tests ./installer/
 cp ./tests/.libs/t38_terminal_tests ./installer/
 cp ./test-data/itu/fax/R1200_1200_A4.tif ./installer/test-data/itu/fax/
@@ -17,6 +18,8 @@ echo 'ln -s /home/protocolEngine/data/codebase/user/bin/spandsp/t38_terminal_tes
 echo 'ln -s /home/protocolEngine/data/codebase/user/bin/spandsp/libspandsp-sim.so.2 /home/protocolEngine/bin/libspandsp-sim.so.2' >> ./installer/install.sh
 echo 'ln -s /home/protocolEngine/data/codebase/user/bin/spandsp/libspandsp.so.2 /home/protocolEngine/bin/libspandsp.so.2' >> ./installer/install.sh
 echo 'ln -s /home/protocolEngine/data/codebase/user/bin/spandsp/test-data faxdocs' >> ./installer/install.sh
+echo 'rpm -i /home/protocolEngine/data/codebase/user/bin/spandsp/*.rpm --nodeps --replacefiles' >> ./installer/install.sh
+echo 'rpm -U /home/protocolEngine/data/codebase/user/bin/spandsp/*.rpm --nodeps --replacefiles' >> ./installer/install.sh
 
 chmod +x ./installer/install.sh
 
